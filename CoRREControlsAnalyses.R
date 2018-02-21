3f4f97acd190e341c9dc8805698f7fabf24c1a51
La Pierre <LaPierreK@SI.EDU>
2018-02-21 16:22:10 -0500

    Lots of exploratory figures.

--- CoRREControlsAnalyses.R
+++ CoRREControlsAnalyses.R
@@ -12,7 +12,7 @@ theme_update(axis.title.x=element_text(size=20, vjust=-0.35, margin=margin(t=15)
 setwd("C:/Users/la pierrek/Dropbox (Smithsonian)/working groups/converge diverge working group/converge_diverge/Control Paper/Output")
 
 #Read in data
-change<-read.csv("Comm_change.csv")%>%
+change<-read.csv("Comm_change_all.csv")%>%
   separate(calendar_year_pair, c("start", "end"), sep="-")
 
 ggplot(data=change, aes(x=end, y=composition_change))+
@@ -20,7 +20,7 @@ ggplot(data=change, aes(x=end, y=composition_change))+
   facet_wrap(~site_project_comm, scales="free_x")
 
 #Read in data
-slope<-read.csv("rate_change_nograze.csv")
+slope<-read.csv("rate_change_all.csv")
 
 meanslope<-slope%>%
   group_by(site_project_comm)%>%
@@ -43,5 +43,21 @@ ggplot(data=ave_int, aes(x=interval, y=meandist))+
   
 
 ggplot(data=rt_change_int, aes(x=interval, y=distance))+
-  geom_point()+
+  geom_boxplot(aes(group=interval))+
   facet_wrap(~site_project_comm, scales="free")
+
+
+#looking at replicates at a single site
+ggplot(data=subset(rt_change_int, site_project_comm=="JSP_GCE_0"), aes(x=interval, y=distance))+
+  geom_boxplot(aes(group=interval))+
+  facet_wrap(~plot_id, scales="free")
+
+
+ave_int_plot<-rt_change_int%>%
+  group_by(site_project_comm, interval, plot_id)%>%
+  summarise(meandist=mean(distance))
+ggplot(data=ave_int_plot, aes(x=interval, y=meandist, group=plot_id))+
+  #geom_smooth(method="lm", formula=y~x+I(x^2), se=F)+
+  geom_line()+
+  facet_wrap(~site_project_comm, scales="free")+
+  theme(legend.position = "none")
