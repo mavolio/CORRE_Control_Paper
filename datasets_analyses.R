f56d887901650b9eb51b53021758f163537fed60
Meghan Avolio <meghanavolio@Meghans-MB.local>
2018-02-21 10:55:19 -0500

    still have a probelm with grazing for rate_change and rate_change_interval.

--- datasets_analyses.R
+++ datasets_analyses.R
@@ -7,13 +7,13 @@ list<-read.csv("C:\\Users\\megha\\Dropbox\\converge_diverge\\Control Paper\\cont
          keep = 1)
 
 ###reading in and cleaning corre data
-corredat<-read.csv("C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\SpeciesRelativeAbundance_Oct2017.csv")%>%
+corredat<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_Oct2017.csv")%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
   filter(site_project_comm!="GVN_FACE_0")%>%
   select(site_code, project_name, community_type, calendar_year, genus_species, relcov, treatment, plot_id, site_project_comm)
 
-corredat_info<-read.csv("C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\ExperimentInformation_Nov2017.csv")%>%
+corredat_info<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/ExperimentInformation_Nov2017.csv")%>%
   select(-X)%>%
   mutate(site_project_comm=paste(site_code, project_name, community_type, sep="_"))%>%
   filter(site_project_comm!="GVN_FACE_0")
@@ -29,11 +29,11 @@ unique(corredat_controls$site_project_comm)
 
 ### reading in and CLEANING CODYN DATASET
 #restrict to species that are present in an experiment
-codyndat<-read.csv('C:\\Users\\megha\\Dropbox\\CoDyn\\R Files\\11_06_2015_v7\\relative cover_nceas and converge_12012015_cleaned.csv')%>%
+codyndat<-read.csv('~/Dropbox/CoDyn/R Files/11_06_2015_v7/relative cover_nceas and converge_12012015_cleaned.csv')%>%
   gather(species, abundance, sp1:sp99)%>%
   filter(site_code!="MISS")
 
-codyndat_info<-read.csv("C:\\Users\\megha\\Dropbox\\CoDyn\\R Files\\11_06_2015_v7\\siteinfo_key.csv")%>%
+codyndat_info<-read.csv("~/Dropbox/CoDyn/R Files/11_06_2015_v7/siteinfo_key.csv")%>%
   filter(site_project_comm!="")
 
 splist<-codyndat%>%
@@ -58,9 +58,9 @@ unique(codyndat_subset$site_project_comm)
 
 
 ### reading in and CLEANING grazing DATASET
-grazing<-read.csv("C:\\Users\\megha\\Dropbox\\lights in the prairie\\GExforCoRREControlMS.csv")%>%
+grazing<-read.csv("~/Dropbox/lights in the prairie/GExforCoRREControlMS.csv")%>%
   mutate(site_project_comm = site, calendar_year = year,
-         plot_id = paste (exage, block, plot, sep = "_"))%>%
+         plot_id = paste (exage, block, plot, sep = ""))%>%
   filter(site_project_comm != "KRNP_Lammertjiesleegte")%>%
   select(site_project_comm, calendar_year, genus_species, relcov, plot_id)
 
@@ -83,20 +83,41 @@ for (i in 1:length(spc)){
   
   mult_change<-rbind(mult_change, out)
 }
-
+write.csv(mult_change, '~/Dropbox/converge_diverge/Control Paper/Output/Comm_change.csv')
 hist(mult_change$composition_change)
 
-#####look at directional change
-spc<-unique(data$site_project_comm)
-rate_change<-data.frame()
+#####look at directional change with intervals #I am not sure which one we want
+nograze<-data<-rbind(codyndat_subset, corredat_controls)
+spc<-unique(nograze$site_project_comm)
+rt_change_int<-data.frame()
 
 for (i in 1:length(spc)){
   
-  subset<-data%>%
+  subset<-nograze%>%
+    filter(site_project_comm==spc[i])
+  
+  out<-rate_change_interval(subset, time.var = 'calendar_year', species.var = "genus_species", abundance.var = 'relcov', replicate.var = 'plot_id')
+  out$site_project_comm<-spc[i]
+  
+  rt_change_int<-rbind(rt_change_int, out)
+}
+write.csv(rt_change_int, '~/Dropbox/converge_diverge/Control Paper/Output/rate_change_interval_nograze.csv')
+#####look at directional change
+nograze<-data<-rbind(codyndat_subset, corredat_controls)
+spc<-unique(nograze$site_project_comm)
+rt_change<-data.frame()
+
+for (i in 1:length(spc)){
+  
+  subset<-nograze%>%
     filter(site_project_comm==spc[i])
   
   out<-rate_change(subset, time.var = 'calendar_year', species.var = "genus_species", abundance.var = 'relcov', replicate.var = 'plot_id')
   out$site_project_comm<-spc[i]
   
-  rate_change<-rbind(rate_change, out)
+  rt_change<-rbind(rt_change, out)
 }
+write.csv(rt_change, '~/Dropbox/converge_diverge/Control Paper/Output/rate_change_nograze.csv')
+
+##trouble shooting grazing datasets
+rate_change(subset(grazing, site_project_comm="KLEE_wildlife"),time.var = 'calendar_year', species.var = "genus_species", abundance.var = 'relcov', replicate.var = 'plot_id')
